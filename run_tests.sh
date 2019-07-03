d26bc8d663464f04b867ea2670b9fcd187f162c5
Sean Abraham <seanabraham@chromium.org>
2019-07-03 09:57:20 -0600

    run_tests: do a "go build" on all executable packages
    This will help prevent compilation errors for go classes that don't have
    tests.
    
    BUG=None
    TEST=run_tests.sh
    
    Change-Id: I77ea73b2ee22dff75b302cae4d8042ec8f30ed69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/infra/go/+/1687921
    Commit-Queue: Sean Abraham <seanabraham@chromium.org>
    Tested-by: Sean Abraham <seanabraham@chromium.org>
    Reviewed-by: Jack Neus <jackneus@google.com>

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# Runs all of the Go tests in this module.
+# Runs all of the Go tests in this module and verifies all binaries compile.
 # This file shouldn't really be necessary. We should be able to just run
 # "go test .", but the module definition isn't quite right at the moment.
 
@@ -16,3 +16,7 @@ for d in $test_dirs; do
   go test $d
 done
 
+echo "Checking that binaries compile"
+go build ./...
+
+echo "Done"
